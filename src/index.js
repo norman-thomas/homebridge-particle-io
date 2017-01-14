4cd1d88b4ccbccc547247da10c93291132625073
Norman Thomas <nt@grin.com>
2017-01-14 16:13:43 +0100

    rename 'homebridge-particle' to new 'homebridge-particle-io' as well as the plugin name Particle to ParticleIO

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const ParticlePlatform = require('./platform.js');
 
 module.exports = (homebridge) => {
   global.homebridge = homebridge;
-  homebridge.registerPlatform('homebridge-particle', 'Particle', ParticlePlatform);
+  homebridge.registerPlatform('homebridge-particle-io', 'ParticleIO', ParticlePlatform);
 };
