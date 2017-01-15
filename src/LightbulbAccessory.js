afdd0fbc0d54e749a1368e47b94c04c577d1abae
Norman Thomas <news@hiroki.de>
2017-01-15 14:30:07 +0100

    issue #3: cleanup + added some missing parameters

--- src/LightbulbAccessory.js
+++ src/LightbulbAccessory.js
@@ -2,10 +2,10 @@ const ActorAccessory = require('./ActorAccessory.js');
 
 class LightbulbAccessory extends ActorAccessory {
 
-  constructor(log, device, homebridge) {
+  constructor(log, url, accessToken, device, homebridge) {
     const Service = homebridge.hap.Service;
     const Characteristic = homebridge.hap.Characteristic;
-    super(log, device, homebridge, Service.Lightbulb, Characteristic.On);
+    super(log, url, accessToken, device, homebridge, Service.Lightbulb, Characteristic.On);
   }
 
   setState(value, callback) {
