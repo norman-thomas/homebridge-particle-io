17af37de56e588f3f57b0f0da65fe0ad5f1136d0
Norman Thomas <news@hiroki.de>
2017-01-15 23:29:12 +0100

    issue #2: more complete test for Platform.js

--- test/Platform-test.js
+++ test/Platform-test.js
@@ -5,11 +5,16 @@ const dummyConfig = require('./dummyConfig.js');
 const dummyHomebridge = require('./dummyHomebridge.js');
 
 const Platform = require('../src/Platform.js');
+const AccessoryFactory = require('../src/AccessoryFactory.js');
+const Accessory = require('../src/Accessory.js');
+const LightbulbAccessory = require('../src/LightbulbAccessory.js');
+const TemperatureSensorAccessory = require('../src/TemperatureSensorAccessory.js');
+const HumiditySensorAccessory = require('../src/HumiditySensorAccessory.js');
 
 global.homebridge = dummyHomebridge;
 
 
-describe('Platform-test', () => {
+describe('Platform.js', () => {
   describe('constructor', () => {
     it('should assign config values to member variables', () => {
       const platform = new Platform(() => {}, dummyConfig);
@@ -17,13 +22,22 @@ describe('Platform-test', () => {
       platform.url.should.be.equal(dummyConfig.cloud_url);
       platform.devices.length.should.be.equal(dummyConfig.devices.length);
       platform.accessoryFactory.should.not.be.undefined;
+      platform.accessoryFactory.should.be.an.instanceOf(AccessoryFactory);
     });
+  });
 
-    it('should report accessories', () => {
+  describe('accessories', () => {
+    it('should report accessories correctly', () => {
       const platform = new Platform(() => {}, dummyConfig);
-      const spy = sinon.spy();
-      platform.accessories(spy);
-      spy.should.have.been.calledOnce;
+      const accessoryCallbackSpy = sinon.spy();
+      platform.accessories(accessoryCallbackSpy);
+      accessoryCallbackSpy.should.have.been.calledOnce;
+      const args = accessoryCallbackSpy.args[0][0];
+      args.length.should.be.equal(4);
+      args.forEach(arg => arg.should.be.an.instanceOf(Accessory));
+      args.map(arg => arg.constructor).should.be.deepEqual(
+        [LightbulbAccessory, LightbulbAccessory, TemperatureSensorAccessory, HumiditySensorAccessory]
+      );
     });
   });
 });
