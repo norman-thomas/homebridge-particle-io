69d803b9180e80578226150623d0892998ac9b1c
Darryl Hodgins <darrylh@darrylh.ca>
2018-05-09 19:36:18 -0700

    Add missing functionName to SwitchAccessory

--- src/SwitchAccessory.js
+++ src/SwitchAccessory.js
@@ -6,10 +6,11 @@ class SwitchAccessory extends ActorAccessory {
     const Service = homebridge.hap.Service;
     const Characteristic = homebridge.hap.Characteristic;
     super(log, url, accessToken, device, homebridge, Service.Switch, Characteristic.On);
+    this.functionName = device.function_name;
   }
 
   setState(value, callback) {
-    super.setState(value ? '1' : '0', callback);
+    super.setState(this.functionName, value ? '1' : '0', callback);
   }
 }
 
